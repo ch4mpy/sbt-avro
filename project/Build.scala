c049d2f544f428d66d1ec6ecb2dbb9bf0e0120fb
ch4mpy <ch4mp@c4-soft.com>
2016-06-27 11:32:29 +0200

    release 1.2.0

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object build extends Build {
       base = file("."),
       settings = Defaults.defaultSettings ++ Seq[Project.Setting[_]](
         organization := "com.c4soft",
-        version := "1.2.0-SNAPSHOT",
+        version := "1.2.0",
         sbtPlugin := true,
         scalaVersion := "2.10.4",
         libraryDependencies ++= Seq(
